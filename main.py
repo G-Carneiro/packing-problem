ba956d4a42eb465e44e75ee124dc24c31f008ca1
Gabriel Carneiro <carneiro.gabr@gmail.com>
2023-03-18 20:24:21 -0300

    export model

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
 from time import time
 
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
 from src.model.item import Item
 from src.utils.order_mode import OrderMode
 from src.utils.split_mode import SplitMode
 
 
+def export_model(model: Item, figure_file: str) -> None:
+    fig, ax = plt.subplots()
+    box = Rectangle(xy=(0, 0), width=model.width, height=model.height, alpha=0.1)
+    ax.add_patch(box)
+    cmap = plt.cm.get_cmap('brg', len(model.items))
+    for idx, item in enumerate(model.items):
+        if item.position is None:
+            continue
+        x = item.position.x
+        y = item.position.y
+        rect = Rectangle(xy=(x, y), width=item.width, height=item.height,
+                         facecolor=cmap(idx), alpha=0.2, linewidth=1, edgecolor='k')
+        ax.add_patch(rect)
+        cx = x + rect.get_width() / 2
+        cy = y + rect.get_height() / 2
+        ax.annotate(f"{idx}", (cx, cy), color='black', weight='bold', fontsize=10, ha='center',
+                    va='center')
+
+    plt.xlim(0, model.width)
+    plt.ylim(0, model.height)
+    plt.savefig(figure_file)
+    plt.close()
+    return None
+
+
 def main() -> None:
-    num_tests: int = 5
+    num_tests: int = 1
     for file_id in range(1, 14):
         file = f"references/bkw/BKW{file_id}"
         with open(file, "r") as f:
@@ -28,6 +56,11 @@ def main() -> None:
                         print(box.regions)
                         for item in box.items:
                             print(item.position)
+                    else:
+                        export_model(model=box, figure_file=f"output/bkw{file_id}_"
+                                                            f"{split.name.lower()}_"
+                                                            f"{order.name.lower()}_"
+                                                            f"{str(descending).lower()}.png")
         exit()
 
 
