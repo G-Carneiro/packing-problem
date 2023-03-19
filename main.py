6aab0a83f1b69c7b7e575e4d9fbe83bf26ab7edf
Gabriel Carneiro <carneiro.gabr@gmail.com>
2023-03-19 13:47:29 -0300

    data as readable table

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from os import path, scandir
 from time import time
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+from pandas import DataFrame
+from tabulate import tabulate
 
 from src.model.item import Item
 from src.utils.order_mode import OrderMode
@@ -33,17 +36,18 @@ def export_model(model: Item, figure_file: str) -> None:
     return None
 
 
-def main() -> None:
+def main(folder: str = "references/bkw") -> None:
     num_tests: int = 1
-    for file_id in range(1, 14):
-        file = f"references/bkw/BKW{file_id}"
+    csv_folder = "output/csv"
+    data_folder = "output/data"
+    for file in scandir(folder):
+        file_name = file.name
         with open(file, "r") as f:
             lines = f.readlines()
         for split in SplitMode:
             for order in OrderMode:
                 for descending in [True, False]:
                     for _ in range(num_tests):
-                        print(order, split, descending)
                         items: list[Item] = []
                         for line in lines[2:]:
                             _, width, height = line.split()
@@ -52,16 +56,43 @@ def main() -> None:
                         box = Item(width=float(width), height=float(height), items=items)
                         start = time()
                         box.solve(order_mode=order, split_mode=split, decrescent=descending)
-                        end = time() - start
-                        print(box.regions)
-                        for item in box.items:
-                            print(item.position)
+                        exec_time = time() - start
+                        data_file = f"{csv_folder}/{file_name}"
+                        data = {"Instance": [file_name],
+                                "OrderMode": [order.name],
+                                "SplitMode": [split.name],
+                                "Descrescent": [descending],
+                                "Exec. Time": [exec_time],
+                                "Solution Quality": [box.solution_quality()],
+                                "Busy %": [box.percent_busy()],
+                                "Free %": [box.percent_free()],
+                                "Nº Items": [len(box.items)],
+                                "Inside Items": [box.inside_items()],
+                                "Outside Items": [box.outside_items()],
+                                "Inside Items %": [box.inside_items_percent()],
+                                "Outside Items %": [box.outside_item_percent()]}
+
+                        with open(f"{data_file}", "a") as f:
+                            DataFrame(data).to_csv(f, index=False,
+                                                   header=not bool(path.getsize(data_file)))
                     else:
-                        export_model(model=box, figure_file=f"output/bkw{file_id}_"
+                        export_model(model=box, figure_file=f"output/figures/{file_name.lower()}_"
                                                             f"{split.name.lower()}_"
                                                             f"{order.name.lower()}_"
                                                             f"{str(descending).lower()}.png")
-        exit()
+        break
+    for file in scandir(csv_folder):
+        file_name = file.name
+        with open(file, "r") as f:
+            lines = f.readlines()
+
+        data = []
+        for line in lines:
+            data.append(line.split(","))
+
+        with open(f"{data_folder}/{file_name}.dat", "w") as f:
+            s = tabulate(tabular_data=data, headers="firstrow", tablefmt="plain")
+            f.write(s)
 
 
 if __name__ == "__main__":
